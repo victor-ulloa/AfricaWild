697e8ce0c8b389dddf930b4ab8af06aecde79358
Victor Ulloa <victor@Victors-MacBook-Pro.local>
2023-11-15 11:13:24 -0500

    project init

---
+++ AfricaWild/AfricaWildApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaWildApp.swift
+//  AfricaWild
+//
+//  Created by Victor Ulloa on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct AfricaWildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
